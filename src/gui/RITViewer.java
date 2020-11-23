d750668f325f2ef78eccefd5bb76b3642c76393d
Evelyn Barnes <ejb3831@rit.edu>
2020-11-22 23:36:23 -0500

    added comments and finishing touches

--- src/gui/RITViewer.java
+++ src/gui/RITViewer.java
@@ -11,14 +11,30 @@ import javafx.stage.Stage;
 import java.util.List;
 
 import gui.RITQuadTree;
+/**
+ * RITViewer is an image rendering tool. the Rich Image Text Viewer or RITViewer 
+ * loads uncompressed txt files into Grayscale images.
+ * @author Evan/Evelyn Barnes
+ * @author Alex Smith
+ */
 public class RITViewer extends Application {
-    
+    /**
+     * the start method is the meat and potatoes of the RIT Viewer,
+     * if you like your meat and potatoes digital, and in the form
+     * of unrelated words. The start function is called to start
+     * the javafx program which displays the image
+     * @param stage the stage on which everything is displayed
+     */
     @Override
     public void start(Stage stage) throws Exception {
+        //get command line arguments
         List<String> args = getParameters().getRaw();
+        //generate a new RITQuadTree(ironicly, does not include any quad trees)
         RITQuadTree qIn = new RITQuadTree(args.get(0));
+        //create the canvas to show pixels
         Canvas pixels = new Canvas(qIn.sideLength-1,qIn.sideLength-1);
         GraphicsContext gc = pixels.getGraphicsContext2D();
+        //iterate through the 2D array of pixel values and draw them
         for(int i = 0; i<qIn.sideLength-1;i++){
             for(int j = 0; j < qIn.sideLength-1;j++){
                 int tVal = qIn.getImage().get(i).get(j);
@@ -34,6 +50,7 @@ public class RITViewer extends Application {
                 
             }
         }
+        //finally show the result, our beutiful meat and potatoes...i mean image
         Group pixelGroup = new Group(pixels);
         Scene scene = new Scene(pixelGroup);
         stage.setScene(scene);
@@ -41,6 +58,10 @@ public class RITViewer extends Application {
         
     }
 
+    /**
+     * The main method, in this case only used to check that the arguments are valid and start the application
+     * @param args the arguments, should be ONLY the file to load
+     */
     public static void main(String[] args) {
         if(args.length!=1){
             System.out.println("Usage: java RITViewer uncompressed.txt");
--- src/ptui/RITUncompress.java
+++ src/ptui/RITUncompress.java
@@ -9,8 +9,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 import model.RITQTNode;
-
+/**
+ * RITUncompress or Rich Image Text Uncompress takes files compressed in the RIT file format
+ * and uncompresses them into regular TXT files
+ * @author Evelyn/Evan Barnes
+ * @author Alex Smith  
+ */
 public class RITUncompress {
+    /**
+     * this class is the uhhh *mental note think of synonym of meat and potatoes* of RITUncompress
+     * it takes in the compressed .RIT file and uncompresses the file into a .TXT file
+     * @param args compressed.rit uncompressed.txt
+     */
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java RITUncompress compressed.rit uncompressed.txt");
@@ -18,6 +28,7 @@ public class RITUncompress {
         }
         System.out.println("Uncompressing: " + args[0]);
         LinkedList<Integer> textNums = new LinkedList<Integer>();
+        //read the .rit file input into the textNums LinkedList
         try {
             Files.readAllLines(Paths.get(args[0])).forEach((x) -> {
                 textNums.add(Integer.valueOf(x));
@@ -27,46 +38,81 @@ public class RITUncompress {
             e.printStackTrace();
             return;
         }
+        //get the size by removing the first member of the List
         int size = textNums.remove(0);
         System.out.print("QTree: ");
+        //Load the data from the list into a RITQTNode quad tree
         RITQTNode quadTree = qtform(textNums);
         System.out.println("");
-        
+        //convert the RITQTNode quad tree into a uncompressed list and save it to a file
         LinkedList<Integer> imAsArr = convertToArr(quadTree, (int) Math.sqrt(size), args[1]); 
         
         
 
     }
     
-
+    /**
+     * Recursively builds the quad tree out of RITQTNodes
+     * @param image the input list
+     * @return the fully formed RITQTNode quad tree
+     */
     public static RITQTNode qtform(List<Integer> image){
+        //get first value of the list
         int value = image.remove(0);
         System.out.print(""+value+", ");
         if(value==-1){
+            //value is a "split" node, recursion continues
             return new RITQTNode(value, qtform(image), qtform(image), qtform(image), qtform(image));
         }else{
+            //value is a leaf node and returns a RITQTNode reflecting that
             return new RITQTNode(value);
         }
     }
-
+    /**
+     * this is another method that im pretty proud of, I was stumped for a couple hours before thinking of this implementation
+     * getNodeVal finds the value of a x,y input by recursively searching the tree and throwing out quadrents it cant be in
+     * only problem this semester that made me get out graph paper and a pencil and draw out the search
+     * 
+     * @param x x coordinate to search for
+     * @param y y coordinate to  search for
+     * @param scope the scope of the search, initial input should be the size of one side of the square
+     * @param tree the RITQTNode tree to search in
+     * @return the value at (x,y)
+     */
     public static Integer getNodeVal(int x, int y, int scope, RITQTNode tree){
+        //shorten the scope to check by half
         int newScope = scope/2;
+        
         if(tree.getVal()!=-1){
+            //found what we were looking for, return that value up the chain
             return tree.getVal();
         }else{
             if((x<=newScope)&&(y<=newScope)){
+                //the point is in the top left quadrent
                 return getNodeVal(x,y,newScope,tree.getUpperLeft());
             }else if((x>newScope)&&(y<=newScope)){
+                //the point is in the top right quadrent
                 return getNodeVal(x-newScope,y,newScope,tree.getUpperRight());
             }else if((x<=newScope)&&(y>newScope)){
+                //the point is in the bottom left quadrent
                 return getNodeVal(x,y-newScope,newScope,tree.getLowerLeft());
             }else{
+                //the point is in the bottom right quadrent
                 return getNodeVal(x-newScope, y-newScope, newScope, tree.getLowerRight());
             }
             
         }
     }
+    /**
+     * the final step of the converstion process, takes the .RIT file, converts it to .TXT file
+     * and then saves that file
+     * @param tree the RITQTNode tree to search
+     * @param size the size of the end image
+     * @param name the name of the file to write too, should be a .rit file
+     * @return the converted array
+     */
     public static LinkedList<Integer> convertToArr(RITQTNode tree, int size, String name){
+        //create the new file to write to
         File unComped = new File(name);
         try{
             if(unComped.createNewFile()){
@@ -82,14 +128,18 @@ public class RITUncompress {
         }
         LinkedList<Integer> imageList = new LinkedList<Integer>();
         try {
+            //open the file in writing mode
             FileWriter writeFile = new FileWriter(unComped);
+            //iterate as if iterating over the uncompressed list
             for(int i=1;i<=size;i++){
                 for(int j=1;j<=size;j++){
+                    //get the value of the tree at j,i. once agian IN COL FIRST FORMAT >:c
                     int tVal = getNodeVal(j,i,size,tree);
                     imageList.add(tVal);
                     writeFile.write(""+tVal+"\n");
                 }
             }
+            //close the file
             writeFile.close(); 
         } catch (IOException e) {
             System.out.println("Error generating output file: ");
